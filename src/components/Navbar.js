01739fdee8dd5197bde2b304bf18779ca611997a
razdechantal <raz.dechantal@gmail.com>
2024-05-17 03:00:21 +0200

    Some fixes

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const Navbar = () => {
+  const { currentUser, logout } = useAuth();
+
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/myposts">My Posts</Link>
+      {currentUser ? (
+        <>
+          <span>{currentUser.email}</span>
+          <button onClick={logout}>Logout</button>
+        </>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
